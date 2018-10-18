87f98b3787c53d0737e1dd3a497a477571652c61
alexandrerf3 <alexandre.ferreira@ccc.ufcg.edu.br>
2018-10-18 11:57:25 -0300

    -update

--- Haskell/prar.hs
+++ Haskell/prar.hs
@@ -3,6 +3,8 @@ import Control.Monad
 import Data.Char
 
 roleta = (100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, "Passou a vez", "Perdeu tudo")
+rodadas = 0
+tema = " "
 
 limpaTela = do
     putStr "\ESC[2J"
@@ -30,13 +32,16 @@ regras = do
     limpaTela
     case1
 
-opcaoInvalida :: IO()
-opcaoInvalida = do
+opcaoInvalida :: Int -> IO()
+opcaoInvalida x = do
+    limpaTela
     putStrLn "***********************************************************************"
     putStrLn "--------------------------- OPCÃO INVÁLIDA ----------------------------"
     putStrLn "***********************************************************************"
     threadDelay 2000000
     limpaTela
+    case x of 1 -> case1
+              2 -> case2
 
 telaInicial :: IO()
 telaInicial = do
@@ -53,27 +58,53 @@ telaInicial = do
  
 telaDeOpcoes :: IO()
 telaDeOpcoes = do
-    putStrLn "-----------------------------------------------------------------------"
-    putStrLn "********************* RODA A RODA JEQUITI *****************************"
-    putStrLn "----------------------  Comecar jogo 1  -------------------------------"
-    putStrLn "-------------------------  Regras 2  ----------------------------------"
-    putStrLn "------------------  Ranking vencedores 3  -----------------------------"
     putStrLn "***********************************************************************"
+    putStrLn "--------------------- RODA A RODA JEQUITI -----------------------------"
+    putStrLn "***********************************************************************"
+    putStrLn "-------- Escolha o número referente a opção que deseja: ---------------"
     putStrLn "-----------------------------------------------------------------------"
+    putStrLn "---------------------- 1. Começar o jogo ------------------------------"
+    putStrLn "---------------------- 2. Visualizar as regras ------------------------"
+    putStrLn "-----------------------------------------------------------------------"
+    putStrLn "***********************************************************************"
     putStrLn "\t"
     putStrLn ">> Qual a sua escolha?"
 
 escolherTema :: IO()
 escolherTema = do
-    putStrLn "-----------------------------------------------------------------------"
-    putStrLn "********************* RODA A RODA JEQUITI *****************************"
-    putStrLn "-------------------------  Geografia 1  -------------------------------"
-    putStrLn "--------------------------  Marcas 2  ---------------------------------"
-    putStrLn "--------------------------  Filmes 3  ---------------------------------"
     putStrLn "***********************************************************************"
+    putStrLn "--------------------- RODA A RODA JEQUITI -----------------------------"
+    putStrLn "***********************************************************************"
+    putStrLn "-------- Escolha o número referente ao tema que deseja: ---------------"
     putStrLn "-----------------------------------------------------------------------"
+    putStrLn "-------------------------- 1. Geografia -------------------------------"
+    putStrLn "-------------------------- 2. Marcas ----------------------------------"
+    putStrLn "-------------------------- 3. Filmes ----------------------------------"
+    putStrLn "-----------------------------------------------------------------------"
+    putStrLn "***********************************************************************"
     putStrLn "\t"
     putStrLn ">> Qual a sua escolha? "
+
+escolherQtdRodadas :: IO()
+escolherQtdRodadas = do
+    putStrLn "***********************************************************************"
+    putStrLn "--------------------- RODA A RODA JEQUITI -----------------------------"
+    putStrLn "***********************************************************************"
+    putStrLn "------------------- /*/ MODIFICAR AINDA /*/ ---------------------------"
+    putStrLn "------------------------- 3 rodadas -----------------------------------"
+    putStrLn "------------------------- 5 rodadas -----------------------------------"
+    putStrLn "------------------------- 9 rodadas -----------------------------------"
+    putStrLn "-----------------------------------------------------------------------"
+    putStrLn "***********************************************************************"
+    putStrLn "\t"
+    putStrLn ">> Qual a sua escolha? "
+
+case3 :: IO()
+case3 = do
+    limpaTela
+    escolherQtdRodadas
+    rodadas <- readLn :: IO Int
+    putStrLn ( show( rodadas))
     threadDelay 2000000
 
 case2 :: IO()
@@ -81,10 +112,9 @@ case2 = do
     limpaTela
     escolherTema
     input <- getLine
-    case input of "1" -> putStr "1"
-                  "2" -> putStr "2"
-                  "3" -> putStrLn "3"
-                  _ -> opcaoInvalida
+    if (input /= "1" && input /= "2" && input /= "3") then opcaoInvalida 2
+    else tema = input
+        
 
 case1 :: IO()
 case1 = do
@@ -92,7 +122,7 @@ case1 = do
     op <- getLine
     case op of "1" -> case2
                "2" -> regras
-               _ -> opcaoInvalida 
+               _ -> opcaoInvalida 1
 
 
 iniciar = do
