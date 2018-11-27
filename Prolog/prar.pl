55a49f043acda752822cf74917d41152f18a185d
JonathanAllisson <jonathan.allisson.silva@ccc.ufcg.edu.br>
2018-11-27 09:29:07 -0300

    Proloucura

--- Prolog/prar.pl
+++ Prolog/prar.pl
@@ -39,16 +39,61 @@ caso3() :-
 caso4():-
     limpaTela,
     menus: comecar,
-    lerString(QtdJogadores),
-    atom_number(QtdJogadores, Y)->(
-        Y >= 1, Y =< 3, assert(qtdJogadores(QtdJogadores));
-        menus:opcaoInvalida, caso4()
-        ),
-    comecar.
+    lerString(Y)->(
+        Y == "1", umJogador([],[],[]);
+        Y == "2", doisJogadores([],[],[]);
+        Y == "3", tresJogadores([],[],[]);
+        Y \= "1", Y \= "2", Y\= "3", menus:opcaoInvalida,caso4
+        ).
     
-comecar():-
-    retract(tema(Tema)),
-    writeln(Tema).
+
+    
+umJogador(J, J2, J3):-
+    writeln("Digite seu nome: "),
+    lerString(Y),
+    insereInicio(Y, J, R1),
+    insereInicio(0, R1, R2),
+    insereInicio("Bot 01", J2, R3),
+    insereInicio("Bot 02", J3, R4),
+    comecar(R2, R3, R4).
+    
+doisJogadores(J, J2, J3):-
+    writeln("Digite seu nome: "),
+    lerString(Y),
+    J.append(Y),
+    J.append(0),
+    writeln("Digite seu nome: "),
+    lerString(Z),
+    J2.append(Z),
+    J2.append(0),
+    J3.append("Bot 02"),
+    J3.append(0),
+    comecar(J, J2, J3).
+    
+tresJogadores(J, J2, J3):-
+    writeln("Digite seu nome: "),
+    lerString(Y),
+    J.append(Y),
+    J.append(0),
+    writeln("Digite seu nome: "),
+    lerString(Z),
+    J2.append(Z),
+    J2.append(0),
+    writeln("Digite seu nome: "),
+    lerString(X),
+    J3.append(X),
+    J3.append(0),
+    comecar(J, J2, J3).
+    
+comecar([H|T], [H2|T2], [H3|T3]):-
+    writeln(H),
+    writeln(T),
+    writeln(H2),
+    writeln(T2),
+    writeln(H3),
+    writeln(T3).
+    
+insereInicio(H, L, [H|L]):- !.
 
 
 main :-
