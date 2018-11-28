d122511db075579ecf849a31957ade7a7638b46d
alexandrerf3 <alexandre.ferreira@ccc.ufcg.edu.br>
2018-11-28 16:05:14 -0300

    -update

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -1,10 +1,11 @@
 :- module(main, [limpaTela/0]).
 :- initialization(main).
 :- use_module(menus).
-:- dynamic palavra/3,(tema/1).
+/*:- dynamic palavra/3,(tema/1).*/
 :- dynamic(rodadas/1).
 :- dynamic(qtdJogadores/1).
-% loucura minha aki, Gasparsa
+
+/*loucura minha aki, Gasparsa
 setup_palavra :-
     reconsult('arquivo.pl').
 limitaPeloTema(Tema, Resultado):-
@@ -23,7 +24,8 @@ palavraRandom:-
     limitaPeloTema(Tema, Resultado),
     escolhePalvra(Resultado, RandomPalavra),
     pegaTexto(RandomPalavra, Texto).
-%%%-----------------------------------------------
+*/
+
 limpaTela() :-
 	shell("clear").
 
@@ -57,50 +59,56 @@ caso3() :-
 caso4():-
     limpaTela,
     menus: comecar,
-    lerString(Y),
-    cadastraJogadores(Y, [], [], []).    
+    lerString(Jogadores),
+    cadastraJogadores(Jogadores).    
 
     
-cadastraJogadores("1", J, J2, J3):-
+cadastraJogadores("1"):-
+    limpaTela,
     writeln("Digite seu nome: "),
-    lerString(Y),
-    insereInicio(Y, J, R1),
-    insereInicio(0, R1, R2),
-    insereInicio("Bot 01", J2, T1),
-    insereInicio(0, T1, T2),
-    insereInicio("Bot 02", J3, P1),
-    insereInicio(0, P1, P2),
-    comecar(R2, T2, P2).
+    lerString(Nome),
+    insereInicio(Nome, [], R1),
+    insereInicio(0, R1, Jogador01),
+    insereInicio("Bot 01", [], R2),
+    insereInicio(0, R2, Jogador02),
+    insereInicio("Bot 02", [], R3),
+    insereInicio(0, R3, Jogador03),
+    comecar(Jogador01, Jogador02, Jogador03).
     
-cadastraJogadores("2", J, J2, J3):-
-    writeln("Digite seu nome: "),
-    lerString(Y),
-    insereInicio(Y, J, R1),
-    insereInicio(0, R1, R2),
-    writeln("Digite seu nome: "),
-    lerString(Z),
-    insereInicio(Z, J2, K1),
-    insereInicio(0, K1, K2),
-    insereInicio("Bot 01", J3, T1),
-    insereInicio(0, T1, T2),
-    comecar(R2, K2, T2).
+cadastraJogadores("2"):-
+    limpaTela,
+    writeln("Jogador 01, digite seu nome: "),
+    lerString(Nome01),
+    insereInicio(Nome01, [], R1),
+    insereInicio(0, R1, Jogador01),
+    limpaTela,
+    writeln("Jogador 02, digite seu nome: "),
+    lerString(Nome02),
+    insereInicio(Nome02, [], R2),
+    insereInicio(0, R2, Jogador02),
+    insereInicio("Bot 01", [], R3),
+    insereInicio(0, R3, Jogador03),
+    comecar(Jogador01, Jogador02, Jogador03).
     
-cadastraJogadores("3", J, J2, J3):-
-    writeln("Digite seu nome: "),
-    lerString(Y),
-    insereInicio(Y, J, R1),
-    insereInicio(0, R1, R2),
-    writeln("Digite seu nome: "),
-    lerString(Z),
-    insereInicio(Z, J2, K1),
-    insereInicio(0, K1, K2),
-    writeln("Digite seu nome: "),
-    lerString(N),
-    insereInicio(N, J3, F1),
-    insereInicio(0, F1, F2),
-    comecar(R2, K2, F2).
+cadastraJogadores("3"):-
+    limpaTela,
+    writeln("Jogador 01, digite seu nome: "),
+    lerString(Nome01),
+    insereInicio(Nome01, [], R1),
+    insereInicio(0, R1, Jogador01),
+    limpaTela,
+    writeln("Jogador 02, digite seu nome: "),
+    lerString(Nome02),
+    insereInicio(Nome02, [], R2),
+    insereInicio(0, R2, Jogador02),
+    limpaTela,
+    writeln("Jogador 03, digite seu nome: "),
+    lerString(Nome03),
+    insereInicio(Nome03, [], R3),
+    insereInicio(0, R3, Jogador03),
+    comecar(Jogador01, Jogador02, Jogador03).
 
-cadastraJogadores(_, _, _, _) :-
+cadastraJogadores(_) :-
     limpaTela,
     menus:opcaoInvalida,
     caso4.
@@ -112,15 +120,14 @@ comecar([H|T], [H2|T2], [H3|T3]):-
     writeln(T2),
     writeln(H3),
     writeln(T3).
-insereInicio(H, L, [H|L]):- !.
 
+insereInicio(H, L, [H|L]):- !.
 
 main :-
     limpaTela,
     menus:telaInicial,
     caso1,
     halt(0).
-    
     
 lerString(X) :-
     read_line_to_string(user_input, X).
