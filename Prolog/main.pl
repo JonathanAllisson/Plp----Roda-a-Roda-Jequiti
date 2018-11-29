6fc2af90687d67575780400a7e16310e1204f7bd
alexandrerf3 <alexandre.ferreira@ccc.ufcg.edu.br>
2018-11-29 18:00:25 -0300

    -update

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -1,8 +1,6 @@
 :- initialization(main).
 :- use_module(menus).
-:- dynamic (tema/1).
-:- dynamic(qtdRodadas/1).
-:- dynamic(qtdJogadores/1).
+:- dynamic tema/1, qtdRodadas/1, qtdJogadores/1, nome01/1, pontos01/1, nome02/1, pontos02/1, nome03/1, pontos03/1, pontosT01/1, pontosT02/1, pontosT03/1.
 
 arquivo(Lines) :-
     open('Palavras.txt', read, Str),
@@ -17,9 +15,17 @@ read_file(Stream,[X|L]) :-
     read(Stream,X),
     read_file(Stream,L).
 
-palavra(W) :-
+palavraDica(Palavra, Dica) :-
     arquivo(L),
-    random_member(W, L).
+    random(0, 60, R),
+    K is (R + 60),
+    itemPorIndice(0, R, L, Palavra),
+    itemPorIndice(0, K, L, Dica).
+
+itemPorIndice(I, N, [H|T], E) :-
+    K is (I + 1) -> (
+        I =:= N, E = H;
+        itemPorIndice(K, N, T, E)).
 
 lista(X) :- X = [50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,"Perdeu tudo","Perdeu tudo","Passou a vez","Passou a vez","Passou a vez"].
 
@@ -61,145 +67,123 @@ caso4():-
     lerString(Jogadores),
     cadastraJogadores(Jogadores).
 
-cadastraJogadores("1"):-
+cadastraJogadores("1") :-
     menus: limpaTela,
     writeln("Digite seu nome: "),
     lerString(Nome),
-    insereInicio(Nome, [], R1),
-    insereInicio(0, R1, Jogador01),
-    insereInicio("Bot 01", [], R2),
-    insereInicio(0, R2, Jogador02),
-    insereInicio("Bot 02", [], R3),
-    insereInicio(0, R3, Jogador03),
-    rodadas(Jogador01, Jogador02, Jogador03, 1).
+    assert(nome01(Nome)),
+    assert(pontos01(0)),
+    assert(nome02("Bot 01")),
+    assert(pontos02(0)), 
+    assert(nome03("Bot 02")),
+    assert(pontos03(0)),
+    rodadas(1).
     
 cadastraJogadores("2"):-
     menus: limpaTela,
     writeln("Jogador 01, digite seu nome: "),
     lerString(Nome01),
-    insereInicio(Nome01, [], R1),
-    insereInicio(0, R1, Jogador01),
     menus: limpaTela,
     writeln("Jogador 02, digite seu nome: "),
     lerString(Nome02),
-    insereInicio(Nome02, [], R2),
-    insereInicio(0, R2, Jogador02),
-    insereInicio("Bot 01", [], R3),
-    insereInicio(0, R3, Jogador03),
-    rodadas(Jogador01, Jogador02, Jogador03, 1).
+    assert(nome01(Nome01)),
+    assert(pontos01(0)), 
+    assert(nome02(Nome02)),
+    assert(pontos02(0)), 
+    assert(nome03("Bot 02")),
+    assert(pontos03(0)),
+    rodadas(1).
     
 cadastraJogadores("3"):-
     menus: limpaTela,
     writeln("Jogador 01, digite seu nome: "),
     lerString(Nome01),
-    insereInicio(Nome01, [], R1),
-    insereInicio(0, R1, Jogador01),
     menus: limpaTela,
     writeln("Jogador 02, digite seu nome: "),
     lerString(Nome02),
-    insereInicio(Nome02, [], R2),
-    insereInicio(0,[], R2),
-    insereInicio(Nome02, [], R2),
-    insereInicio(0, R2, Jogador02),
     menus: limpaTela,
     writeln("Jogador 03, digite seu nome: "),
     lerString(Nome03),
-    insereInicio(Nome03, [], R3),
-    insereInicio(0, R3, Jogador03),
-    rodadas(Jogador01, Jogador02, Jogador03, 1).
+    assert(nome01(Nome01)),
+    assert(pontos01(0)), 
+    assert(nome02(Nome02)),
+    assert(pontos02(0)), 
+    assert(nome03(Nome03)),
+    assert(pontos03(0)),
+    rodadas(1).
 
 cadastraJogadores(_) :-
     menus: limpaTela,
     menus:opcaoInvalida,
     caso4.
-    
-rodadas(Jogador01, Jogador02, Jogador03, NumeroRodada) :-
+
+rodadas(NumeroRodada) :-
     qtdRodadas(X) ->
         (NumeroRodada > X, rodadaFinal;
-        palavra(Palavra),
-        pegarPontuacaoNome(Jogador01, _, NJ1),
-        pegarPontuacaoNome(Jogador02, _, NJ2),
-        pegarPontuacaoNome(Jogador03, _, NJ3),
-        writeln(Palavra),
-        sleep(10),
-        jogadas(1, [NJ1, 0], [NJ2, 0], [NJ3, 0], NumeroRodada, "jjj", "j j j", 0, 0, 0),
+        palavraDica(Palavra, Dica),
+        string_chars(Palavra, Chars),
+        cobrirPalavra(Chars, Res),
+        atomic_list_concat(Res, PalavraCoberta),
+        assert(pontosT01(0)), assert(pontosT02(0)), assert(pontosT03(0)),
+        jogadas(1, NumeroRodada, PalavraCoberta, Palavra, Dica, []),
         write(">> Rodada N°: "), writeln(NumeroRodada),
         menus: pontuacaoGeral,
-        pegarPontuacaoNome(Jogador01, P1, N1),
-        pegarPontuacaoNome(Jogador02, P2, N2),
-        pegarPontuacaoNome(Jogador03, P3, N3),
-        write("Pontuação do jogador(a) "), write(N1), write(": "), writeln(P1),
-        write("Pontuação do jogador(a) "), write(N2), write(": "), writeln(P2),
-        write("Pontuação do jogador(a) "), write(N3), write(": "), writeln(P3), 
+        nome01(Nome01), nome02(Nome02), nome03(Nome03),
+        pontos01(P1), pontos02(P2), pontos03(P3),
+        write("Pontuação do jogador(a) "), write(Nome01), write(": "), writeln(P1),
+        write("Pontuação do jogador(a) "), write(Nome02), write(": "), writeln(P2),
+        write("Pontuação do jogador(a) "), write(Nome03), write(": "), writeln(P3), 
         sleep(1),
         Aux is (NumeroRodada + 1),
-        rodadas(Jogador01, Jogador02, Jogador03, Aux)).
+        rodadas(Aux)).
 
-jogadas(Vez, [H1|[HT1|_]], [H2|[HT2|_]], [H3|[HT3|_]], NumeroRodada, PalavraCoberta, Letras, PJ1, PJ2, PJ3) :- 
-    tema(T),
+jogadas(Vez, NumeroRodada, PalavraCoberta, Palavra, Dica, Letras) :-
+    tema(Tem),
     menus: limpaTela,
+    alteraVez(Vez, Nvez),
+    nome01(Nome01), nome02(Nome02), nome03(Nome03),
+    pontosT01(PT1), pontosT02(PT2), pontosT03(PT3),
     write(">> Rodada N°: "), writeln(NumeroRodada),
     writeln("*************************** Pontuação *********************************"),
-    write(H1), write(": "), write(HT1), write(" pontos    ||    "),
-    write(H2), write(": "), write(HT2), write(" pontos    ||    "),
-    write(H3), write(": "), write(HT3), writeln(" pontos"),
+    write(Nome01), write(": "), write(PT1), write(" pontos    ||    "),
+    write(Nome02), write(": "), write(PT2), write(" pontos    ||    "),
+    write(Nome03), write(": "), write(PT3), writeln(" pontos"),
     writeln("***********************************************************************"),
-    write("Tema: "), writeln(T),
-    alteraVez(Vez, Nvez),
-    writeln("DICA - IMPLENTAR"),
+    write("Tema: "), writeln(Tem),
+    write("Dica: "), writeln(Dica),
     write("Palavra: "), writeln(PalavraCoberta),
-    writeln("Letra(s) já escolhida(s): "),
+    write("Letra(s) já escolhida(s): "), writeln(Letras),
     writeln("Girando a roleta..."),
     string_chars("$####$", Chars),
     qntCoberta(Chars, C),
     sleep(2),
     roleta(R),
-    nomeDaVez(Vez, H1, H2, H3, N) -> (
+    nomeDaVez(Vez, Nome) -> (
         C < 3,
         C > 0,
-        write("Valendo "), write(R), write(" pontos por letra restante, "), write(N), writeln(" digite a palavra corretamente:")
+        write("Valendo "), write(R), write(" pontos por letra restante, "), write(Nome), writeln(" digite a palavra corretamente:")
         ;
         C > 3 -> (
             R == "Perdeu tudo",
             writeln("Perdeu tudo..."),
-            zeraPontuacao(Vez, HT1, HT2, HT3, NP1, NP2, NP3), 
+            zeraPontuacao(Vez), 
             sleep(2), 
-            jogadas(Nvez, [H1|[NP1]], [H2|[NP2]], [H3|[NP3]], NumeroRodada, PalavraCoberta, Letras, PJ1, PJ2, PJ3);
+            jogadas(Nvez, NumeroRodada, PalavraCoberta, Palavra, Dica, Letras);
             R == "Passou a vez",
             writeln("Passou a vez..."),
             sleep(2),
-            jogadas(Nvez, [H1|[HT1]], [H2|[HT2]], [H3|[HT3]], NumeroRodada, PalavraCoberta, Letras, PJ1, PJ2, PJ3);
-            write(">> Valendo "), write(R), write(" pontos por letra, "), write(N), writeln(" digite uma letra:"),
+            jogadas(Nvez,NumeroRodada, PalavraCoberta, Palavra, Dica, Letras);
+            write(">> Valendo "), write(R), write(" pontos por letra, "), write(Nome), writeln(" digite uma letra:"),
             sleep(2),
-            alterarPontuacao(Vez, HT1, HT2, HT3, R, NP1, NP2, NP3),
-            jogadas(Nvez, [H1|[NP1]], [H2|[NP2]], [H3|[NP3]], NumeroRodada, PalavraCoberta, Letras, PJ1, PJ2, PJ3))).
+            alterarPontuacao(Vez, R),
+            jogadas(Nvez, NumeroRodada, PalavraCoberta, Palavra, Dica, Letras))).
 
-alteraVez(Vez, R) :-
-    Vez =:= 1, R is 2;
-    Vez =:= 2, R is 3;
-    R is 1.
+descobrirPalavra([], [], []).
+descobrirPalavra(L, [PH|PT], [PCH|PCT], [H|T]) :-
+    L == PCH, H = PCH, descobrirPalavra(PT, PCT, T).
 
-nomeDaVez(Vez, H1, H2, H3, R) :- 
-    Vez =:= 1, R = H1;
-    Vez =:= 2, R = H2;
-    R = H3.
-
-zeraPontuacao(Vez, P1, P2, P3, NP1, NP2, NP3) :-
-    Vez =:= 1, NP1 is 0, NP2 = P2, NP3 = P3;
-    Vez =:= 2, NP1 = P1, NP2 is 0, NP3 = P3;
-    NP1 = P1, NP2 = P2, NP3 is 0.
-
-alterarPontuacao(Vez, P1, P2, P3, N, NP1, NP2, NP3) :-
-    Vez =:= 1, K1 is (P1 + N), NP1 = K1, NP2 = P2, NP3 = P3;
-    Vez =:= 2, K2 is (P2 + N), NP1 = P1, NP2 = K2, NP3 = P3;
-    K3 is (P3 + N), NP1 = P1, NP2 = P2, NP3 = K3.
-
-rodadaFinal :-
-    writeln("H89SH89ASHAS").
-
-pegarPontuacaoNome([H|[Ht|_]], H, Ht).
-
-insereInicio(H, L, [H|L]).
+lerString(X) :-
+    read_line_to_string(user_input, X).
 
 cobre(' ', " ").
 cobre(_, "#").
@@ -214,11 +198,31 @@ qntCoberta([H|T], K):-
         H == '#', K is Q + 1;
         K is Q).
 
+alteraVez(Vez, R) :-
+    Vez =:= 1, R is 2;
+    Vez =:= 2, R is 3;
+    R is 1.
+
+nomeDaVez(Vez, R) :- 
+    Vez =:= 1, nome01(R);
+    Vez =:= 2, nome02(R);
+    nome03(R).
+
+zeraPontuacao(Vez) :-
+    Vez =:= 1, retract(pontosT01(_)), assert(pontosT01(0));
+    Vez =:= 2, retract(pontosT02(_)), assert(pontosT02(0));
+    retract(pontosT03(_)), assert(pontosT03(0)).
+
+alterarPontuacao(Vez, N) :-
+    Vez =:= 1, retract(pontosT01(P1)), K1 is (P1 + N), assert(pontosT01(K1));
+    Vez =:= 2, retract(pontosT02(P2)), K2 is (P2 + N), assert(pontosT02(K2));
+    retract(pontosT03(P3)), K3 is (P3 + N), assert(pontosT03(K3)).
+
+rodadaFinal :-
+    writeln("H89SH89ASHAS").
+
 main :-
     menus: limpaTela,
     menus:telaInicial,
     caso1,
     halt(0).
-    
-lerString(X) :-
-    read_line_to_string(user_input, X).
