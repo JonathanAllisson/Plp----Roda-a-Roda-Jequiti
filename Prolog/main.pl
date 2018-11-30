7f453248d0acde3e14934ee401ccccdfd5481d47
Gasparsa <34282336+Gasparsa@users.noreply.github.com>
2018-11-30 01:24:56 -0300

    Update main.pl

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -45,7 +45,7 @@ itemPorIndice(I, N, [H|T], E) :-
         itemPorIndice(K, N, T, E)).
 
 lista(X) :- X = [50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,"Perdeu tudo","Perdeu tudo","Passou a vez","Passou a vez","Passou a vez"].
-
+letras_bot(Y) :- Y["A","E","I","O","U"].
 roleta(Y):-
     lista(X),
     random_member(Y, X).
